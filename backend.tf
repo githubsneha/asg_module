364ca2c6ebbaefba418a7f11489789a917b74bf7
codvatechlabs <78929919+codvatechlabs@users.noreply.github.com>
2022-03-19 05:03:07 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "ctl-terraform-state-ap-south-1"
-    key            = "asg-demo/asg-tf"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-state"
 
-  }
-}
