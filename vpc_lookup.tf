e411a9af9d0d8a6fc17cda375432fd4e321f51e9
githubsneha <89030338+githubsneha@users.noreply.github.com>
2022-11-13 20:45:30 +0530

    Update vpc_lookup.tf

--- vpc_lookup.tf
+++ vpc_lookup.tf
@@ -1,14 +1,14 @@
 data "aws_vpc" "selected" {
   filter {
-    name   = "Name"
+    name   = "tag:Name"
     values = ["main"]
   }
 }
 
 data "aws_subnets" "selected" {
   filter {
-    name   = "VPC ID"
-    values = ["vpc-0d28c55705d08c5c6"]
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
   }
 }
 
